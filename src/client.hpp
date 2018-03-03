56dcf4aed9a3c812529eeb2abbe0f1b58cc3544b
ajberchek <ajberchek@gmail.com>
2018-03-02 23:45:55 -0800

    Added functionality for HTTP put

--- src/client.hpp
+++ src/client.hpp
@@ -105,6 +105,12 @@ private:
                              std::experimental::optional<mtx::client::errors::ClientError>)>,
           bool requires_auth = true);
 
+        template<class Request>
+        void put(const std::string &endpoint,
+                 const Request &req,
+                 std::function<void(std::experimental::optional<mtx::client::errors::ClientError>)>,
+                 bool requires_auth = true);
+
         template<class Response>
         void get(const std::string &endpoint,
                  std::function<void(const Response &,
@@ -170,6 +176,36 @@ mtx::client::Client::post(
         std::shared_ptr<Session> session = create_session<Response, CallbackType>(callback);
 
         session->request.method(boost::beast::http::verb::post);
+        session->request.target("/_matrix/client/r0" + endpoint);
+        session->request.set(boost::beast::http::field::user_agent, "mtxclient v0.1.0");
+        session->request.set(boost::beast::http::field::content_type, "application/json");
+        session->request.set(boost::beast::http::field::host, session->host);
+        if (requires_auth && !access_token_.empty())
+                session->request.set(boost::beast::http::field::authorization,
+                                     "Bearer " + access_token_);
+        session->request.body() = j.dump();
+        session->request.prepare_payload();
+
+        do_request(session);
+}
+
+template<class Request>
+void
+mtx::client::Client::put(
+  const std::string &endpoint,
+  const Request &req,
+  std::function<void(std::experimental::optional<mtx::client::errors::ClientError>)> callback,
+  bool requires_auth)
+{
+        // Serialize request.
+        nlohmann::json j = req;
+
+        using CallbackType =
+          std::function<void(std::experimental::optional<mtx::client::errors::ClientError>)>;
+
+        std::shared_ptr<Session> session = create_session<CallbackType>(callback);
+
+        session->request.method(boost::beast::http::verb::put);
         session->request.target("/_matrix/client/r0" + endpoint);
         session->request.set(boost::beast::http::field::user_agent, "mtxclient v0.1.0");
         session->request.set(boost::beast::http::field::content_type, "application/json");
